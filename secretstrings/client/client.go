beae548ac3fdb569145330ebbdf1e407ea001bb0
Tom B <do20567@bristol.ac.uk>
2021-10-15 10:41:01 +0100

    Message reversing

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,25 @@
 package main
+
 import (
-//	"net/rpc"
 	"flag"
-//	"bufio"
-//	"os"
-//	"secretstrings/stubs"
+	"net/rpc"
+
+	//	"bufio"
+	//	"os"
 	"fmt"
+	"secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+	request := stubs.Request{Message: "Hello"}
+	response := new(stubs.Response)
+	client.Call(stubs.PremiumReverseHandler, request, response)
+	fmt.Println("Responded: " + response.Message)
+
 	//TODO: connect to the RPC server and send the request(s)
 }
